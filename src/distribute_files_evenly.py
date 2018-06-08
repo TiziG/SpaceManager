9d4bf232ae45e88d23ba458c9c863b3e7f90d6e6
Tiziano Gemma <git@tizl.ch>
2018-06-08 02:20:46 +0200

    added a config option to specify nr of distribution runs

--- src/distribute_files_evenly.py
+++ src/distribute_files_evenly.py
@@ -7,16 +7,17 @@ from config import CategoryAndVolumeDefinitions
 from distribute_files_evenly_core import Distributer
 from space_manager_helpers import Logger
 
-_n_tuple_config = namedtuple('config', 'fullest_threshold')
+_n_tuple_config = namedtuple('config', 'fullest_threshold nr_of_runs')
 
 # configuration-----------
 TEST_RUN = False
 LOGGING = True
-CONFIG = _n_tuple_config(0.75)
+CONFIG = _n_tuple_config(0.75, 1)
 # ------------------------
 
 
 if __name__ == '__main__':
-    distributer = Distributer(config=CONFIG, test_run=TEST_RUN, logger=Logger(LOGGING))
-    for category in CategoryAndVolumeDefinitions.categories.categories:
-        distributer.distribute(category)
+    for _ in range(CONFIG.nr_of_runs):
+        distributer = Distributer(config=CONFIG, test_run=TEST_RUN, logger=Logger(LOGGING))
+        for category in CategoryAndVolumeDefinitions.categories.categories:
+            distributer.distribute(category)
