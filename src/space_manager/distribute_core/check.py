6cb73f19ff4e295aad075082d261101252f086af
Tiziano Gemma <git@tizl.ch>
2018-06-09 03:53:49 +0200

    fixed bug where the check condition results were always wrong

--- src/space_manager/distribute_core/check.py
+++ src/space_manager/distribute_core/check.py
@@ -7,7 +7,7 @@ class Check(object):
         fullest_used = fullest_usage.used / fullest_usage.total
         config.logger.log("The Fullest disk is at %d%%. Threshold is %d%%" % (
             fullest_used * 100, config.fullest_threshold * 100))
-        threshold_met = fullest_used < config.fullest_threshold
+        threshold_met = fullest_used > config.fullest_threshold
         config.logger.log_condition(threshold_met)
         return threshold_met
 
@@ -18,6 +18,6 @@ class Check(object):
             Converter.b_to_gb(minimum_free_needed)))
         config.logger.log("actual free space: %dGB" % (
             Converter.b_to_gb(emptiest_free)))
-        enough_space = emptiest_free < minimum_free_needed
+        enough_space = emptiest_free > minimum_free_needed
         config.logger.log_condition(enough_space)
         return enough_space
