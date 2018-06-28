6dbc71d74cf2fb0e2cff7c2a17c327e7055f7e43
Tiziano Gemma <tiziano@gemma-mail.ch>
2018-06-28 14:51:55 +0200

    disabled sonarr-jail integration

--- src/space_manager/smlib/plugins/sonarr.py
+++ src/space_manager/smlib/plugins/sonarr.py
@@ -1,7 +1,5 @@
 # sonarr.py
 
-from subprocess import call
-
 from space_manager.smlib.helpers.logger import Logger
 
 
@@ -13,7 +11,8 @@ class Sonarr(object):
             logger.log('test run: warden stop sonarr_2')
         else:
             logger.log('stopping sonarr...')
-            call(["warden", "stop", "sonarr_2"])
+            # TODO implement sonarr function with new docker setup
+            # call(["warden", "stop", "sonarr_2"])
             logger.line()
             logger.log('...stopping sonarr done')
 
@@ -23,5 +22,6 @@ class Sonarr(object):
             logger.log('test run: warden start sonarr_2')
         else:
             logger.log('starting sonarr...')
-            call(["warden", "start", "sonarr_2"])
+            # TODO implement sonarr function with new docker setup
+            #call(["warden", "start", "sonarr_2"])
             logger.log('...starting sonarr done')
